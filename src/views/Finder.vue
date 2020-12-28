b6fc0660ae37508f2f79153bce53fb0ac146c3d4
Gabriele Giammatteo <gabriele.giammatteo@eng.it>
2020-12-28 09:43:10 +0100

    first version of Finder

--- src/views/Finder.vue
+++ src/views/Finder.vue
@@ -1,22 +1,55 @@
 <template>
   <div class="home">
-    this is the finder
+    <el-input placeholder="Please input" v-model="input"></el-input>
+    <el-button @click="add()">+</el-button>
     <el-button @click="close()">X</el-button>
   </div>
+  <div v-for="i in matchingItems" :key="i.id">{{i.title}}</div>
 </template>
 
 <script>
 // @ is an alias to /src
 
 import { remote } from 'electron'
+import { mapGetters } from 'vuex'
+import storeMixin from '@/components/mixins/store'
+import { MItem } from '@/common/model/mitem'
 
 export default {
+  mixins: [storeMixin],
   name: "Home",
   components: {
+  },
+  data() {
+    return {
+      input: ""
+    }
+  },
+  computed: {
+    ...mapGetters(['allItems']),
+
+    matchingItems() {
+      if (this.input) {
+      return this.allItems.filter(i => {
+        console.log(i.title)
+        console.log(this.input)
+        if (i.title){
+          return i.title.toLowerCase().startsWith(this.input.toLowerCase())
+        }
+        return false
+      })
+    }
+    return []
+    },
   },
   methods: {
     close() {
       remote.getCurrentWindow().close()
+    },
+    add() {
+      let item = new MItem()
+      item.title = this.input
+      this.saveItem(item)
     }
   }
 };
