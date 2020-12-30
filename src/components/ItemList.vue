ffc5d63a63519f2ec626c30d031ca03347aad2d9
Gabriele Giammatteo <gabriele.giammatteo@eng.it>
2020-12-30 10:48:35 +0100

    group by project in the itemlist

--- src/components/ItemList.vue
+++ src/components/ItemList.vue
@@ -1,16 +1,34 @@
 <template>
   <div class="item-list" :class="{ highlighted: highlighted }">
-    <draggable
-    :list="visibleItems"
-    group="people"
-    @change="log"
-    item-key="id">
-    <template #item="{element}">
-      <div>
-        <item :item="element"></item>
-      </div>
-     </template>
-   </draggable>
+    <div v-if="groupByProject && groupedItems.length > 1">
+   <div v-for="group in groupedItems" :key="group.groupName">
+     <el-divider content-position="left">{{ group.groupName === 'nogroup' ? 'Others' : group.groupName }}</el-divider>
+     <draggable
+     :list="group.items"
+     group="people"
+     @change="log"
+     item-key="id">
+     <template #item="{element}">
+       <div>
+         <item :item="element"></item>
+       </div>
+      </template>
+    </draggable>
+   </div>
+ </div>
+ <div v-else>
+   <draggable
+   :list="visibleItems"
+   group="people"
+   @change="log"
+   item-key="id">
+   <template #item="{element}">
+     <div>
+       <item :item="element"></item>
+     </div>
+    </template>
+  </draggable>
+ </div>
 </div>
 </template>
 <script>
@@ -20,7 +38,6 @@ import { v4 as uuidv4 } from 'uuid'
 import Item from '@/components/Item'
 import { MItem } from '@/common/model/mitem'
 import storeMixin from '@/components/mixins/store'
-import { getWeek } from 'date-fns'
 export default {
   mixins: [storeMixin],
   components: {
@@ -35,6 +52,14 @@ export default {
     itemProps: {
       type: Object,
       default: () =>{ return {}}
+    },
+    canDrop: {
+      type: Boolean,
+      default: true
+    },
+    groupByProject: {
+      type: Boolean,
+      default: false
     }
   },
   data(){
@@ -45,6 +70,21 @@ export default {
   },
   computed: {
     ...mapGetters(['topLevelItems']),
+    groupedItems() {
+      let res = {}
+      this.visibleItems.forEach(i => {
+        if (i.project) {
+          res[i.project] ? res[i.project].push(i) : res[i.project] = [i]
+        } else {
+          res['nogroup'] ? res['nogroup'].push(i) : res['nogroup'] = [i]
+        }
+      })
+      return Object.entries(res).map(e => {return {groupName: e[0], items: e[1]}}).sort((a, b) => {
+        if (a.groupName === 'nogroup') return 1
+        if (b.groupName === 'nogroup') return -1
+        return a.groupName.localeCompare(b.groupName)
+      })
+    },
     visibleItems() {
       return this.topLevelItems
         .filter(t => {
@@ -56,9 +96,8 @@ export default {
         })
     },
     highlighted() {
-        if ('week' in this.itemFilters) {
-          let currWeek = getWeek(new Date(), {weekStartsOn: 1})
-          return this.itemFilters.week(currWeek)
+        if ('deadline' in this.itemFilters) {
+          return this.itemFilters.deadline(new Date())
         }
         if ('start' in this.itemFilters) {
           return this.itemFilters.start(new Date())
@@ -72,6 +111,7 @@ export default {
     },
     log(event) {
       if(event.added){
+        if(this.canDrop) {
         if(event.added.element.id){
           this.updateItem(event.added.element.id, this.itemProps)
         } else {
@@ -81,6 +121,7 @@ export default {
           this.saveItem(item)
         }
         //this.updateItem(event.added.element.id, this.itemProps)
+      }
       }
     },
   }
@@ -95,6 +136,6 @@ export default {
     text-decoration: line-through;
 }
 .highlighted {
-  background-color: rgb(238, 255, 237);
+  border: 2px dashed green;
 }
 </style>
