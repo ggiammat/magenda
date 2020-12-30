8dba3a073e4b03931b212e01c8bdbb5452ab139a
Gabriele Giammatteo <gabriele.giammatteo@eng.it>
2020-12-30 10:47:11 +0100

    refactoring of mitem model

--- src/common/model/mitem.js
+++ src/common/model/mitem.js
@@ -11,12 +11,12 @@ const mItemProxyGetter = function(obj, prop) {
 
   if (MERGING_PROPS.includes(prop)) {
     var bodyRes
-    if (obj._props[`ignoredbody_${prop}`]) {
-      bodyRes = obj._bodyprops[prop].filter(e => !obj._props[`ignoredbody_${prop}`].includes(e))
+    if (obj._serializable[`ignoredbody_${prop}`]) {
+      bodyRes = obj._serializable.bodyProps[prop].filter(e => !obj._serializable[`ignoredbody_${prop}`].includes(e))
     } else {
-      bodyRes = obj._bodyprops[prop]
+      bodyRes = obj._serializable.bodyProps[prop]
     }
-    let res = [...bodyRes || [], ...obj._props[prop] || [] ]
+    let res = [...bodyRes || [], ...obj._serializable[prop] || [] ]
     return res
   }
 
@@ -34,44 +34,37 @@ const mItemProxyGetter = function(obj, prop) {
     return res
   }
   */
-
-  return (
-    obj[prop] ||
-    obj._props[prop] ||
-    obj._bodyprops[prop] ||
-    (obj._encapsulated && !NOT_INHERITABLE_PROPS.includes(prop)
-      ? obj._encapsulated[prop]
-      : undefined) ||
-    (obj._extends && !EXTENDS_NOT_INHERITABLE_PROPS.includes(prop)
-      ? obj._extends[prop]
-      : undefined) ||
-    undefined
-  )
+  if (obj[prop] !== undefined) return obj[prop]
+  if (obj._serializable[prop] !== undefined) return obj._serializable[prop]
+  if (obj._serializable.bodyProps[prop] !== undefined) return obj._serializable.bodyProps[prop]
+  if (obj._encapsulated && !NOT_INHERITABLE_PROPS.includes(prop) && obj._encapsulated[prop] !== undefined) return obj._encapsulated[prop]
+  if (obj._extends && !EXTENDS_NOT_INHERITABLE_PROPS.includes(prop) && obj._extends[prop] !== undefined) return obj._extends[prop]
+  return undefined
 }
 
 const mItemProxySetter = function(obj, prop, value) {
   if (prop === 'body'){
-    obj._props[prop] = value
-    //obj._bodyprops['subItems'] = getSubItems(value)
-    obj._bodyprops = extractBodyProps(value)
+    obj._serializable.body = value
+    obj._serializable.bodyProps = extractBodyProps(value)
     return true
   }
 
   //console.log('MItem Proxy Set', prop, value)
   if (MERGING_PROPS.includes(prop)) {
-    if (obj._bodyprops[prop]) {
+    if (obj._serializable.bodyProps[prop]) {
       if (prop === "subItems") {
-        obj._props[prop] = value.filter(e => typeof e === 'string')
+        obj._serializable.bodyProps[prop] = value.filter(e => typeof e === 'string')
       } else {
-        obj._props[prop] = value.filter(e => !obj._bodyprops[prop].includes(e))
-        obj._props[`ignoredbody_{prop}`] = obj._bodyprops[prop].filter(e => !value.includes(e))
+        obj._serializable.bodyProps[prop] = value.filter(e => !obj._serializable.bodyProps[prop].includes(e))
+        obj._props[`ignoredbody_{prop}`] = obj._serializable.bodyProps[prop].filter(e => !value.includes(e))
       }
       return true
     }
   }
 
-  if (Object.prototype.hasOwnProperty.call(obj._props, prop)) {
-    obj._props[prop] = value
+  if (Object.prototype.hasOwnProperty.call(obj._serializable, prop)) {
+    obj._serializable[prop] = value
+    return true
   }
 
 
@@ -87,12 +80,14 @@ const proxyHandler = {
   set: mItemProxySetter,
 
   // REACTIVITY SEEMS TO WORK ALSO WITHOUT THESE TWO TRAPS
+  /*
   has(obj, prop) {
     return this.ownKeys(obj).includes(prop)
   },
   ownKeys: function(obj) {
     return [... new Set([...Object.keys(obj), ...Object.keys(obj._props), ...Object.keys(obj._bodyprops)])]
   },
+  */
 
   getOwnPropertyDescriptor: function (obj, prop) {
     //console.log('PROXY PROPDESCR')
@@ -109,40 +104,40 @@ const proxyHandler = {
 
 export class MItem {
 
-  _props = {
+  _serializable = {
     id: undefined,
     type: undefined,
     title: undefined,
-    week: undefined,
     done: undefined,
     start: undefined,
     source: undefined,
     sourceId: undefined,
-    rels: undefined
+    rels: undefined,
+    body: undefined,
+    bodyRef: undefined,
+    bodyProps: {},
+    deadline: undefined
   }
-
-  _bodyprops = {}
 
   constructor(props = {}) {
-    this.version = 1
-    this._props = {...this._props, ...props}
+    this._serializable = {...this._serializable, ...props}
     const proxy = new Proxy(this, proxyHandler)
-    if ('body' in props) {
-      proxy.body = props.body
+    if(!this.bodyProps && props.body) {
+        proxy.body = props.body
     }
+    //Object.entries(props).filter(e => e[1]).forEach(e => proxy[e[0]] = e[1])
     return proxy
   }
-  static deserialize(item) {
-    return new MItem(item._props)
+  static deserialize(props) {
+    return new MItem(props)
   }
 
   getSerializedProps(){
-    return this.serialize()._props
+    console.warn('Using deprecated getSerializedProps()')
+    return this.serialize()
   }
 
   serialize() {
-    return {
-      _props: _.cloneDeep(this._props)
-    }
+    return _.cloneDeep(this._serializable)
   }
 }
