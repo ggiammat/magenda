111a94528998505643f3ffc9058eca79e7947c97
Gabriele Giammatteo <gabriele.giammatteo@eng.it>
2021-01-05 15:25:09 +0100

    remove unused file

--- src/common/model/test.mjs
+++
@@ -1,8 +0,0 @@
-import { MItem } from './mitem.mjs'
-
-
-const mi = MItem.buildFromDict({id: '9999', title: 'asd'})
-const ser = mi.serialize()
-console.log(mi)
-console.log(ser)
-console.log(MItem.deserialize(ser))
